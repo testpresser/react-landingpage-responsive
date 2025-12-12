c9626217cf2936c736d250a6f18ae73266fa7f92
michael brimmer <michaelbrimmer@michaels-MacBook-Air.local>
2025-12-12 18:10:03 +0000

    Fix TypeScript build errors for Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,8 @@ import { Services } from './components/sections/Services';
 import { AboutUs } from './components/sections/AboutUs';
 import { Pricing } from './components/sections/Pricing';
 import { CTA } from './components/sections/CallToAction';
-import { TrippyScroll } from './components/sections/ScrollOptical';
 import { BackgroundVideo } from "./components/sections/BackgroundVideo";
 import { ContentSplit } from './components/sections/ContentSplit';
-import { TwoDroids } from './components/sections/TwoDroids';
-import { DroidAssasin } from './components/sections/DroidAssasin';
 import { DroidVideo } from './components/sections/DroidVideo';
 import { ThreeDroids } from './components/sections/Three-container';
 
